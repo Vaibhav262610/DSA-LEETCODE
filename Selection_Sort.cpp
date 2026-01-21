18dfd6b7a9a6ce1363016f1d675e9097b66b3240
Vaibhav262610 <branda56235@gmail.com>
2026-01-22 00:53:34 +0530

    hdhdsh

---
+++ Selection_Sort.cpp
@@ -0,0 +1,46 @@
+// class Solution
+// {
+// public:
+//     vector<int> sortArray(vector<int> &nums)
+//     {
+//         for (int i = 0; i < nums.size(); i++)
+//         {
+//             for (int j = i; j < nums.size(); j++)
+//             {
+//                 if (nums[j] < nums[i])
+//                 {
+//                     int temp = nums[i];
+//                     nums[i] = nums[j];
+//                     nums[j] = temp;
+//                 };
+//             };
+//         };
+//         return nums;
+//     };
+// };
+
+// class Solution {
+// public:
+//     vector<int> sortArray(vector<int>& nums) {
+
+//         int n = nums.size();
+
+//         for (int i = 0; i < n - 1; i++) {
+
+//             int minIndex = i;
+
+//             for (int j = i + 1; j < n; j++) {
+//                 if (nums[j] < nums[minIndex]) {
+//                     minIndex = j;
+//                 }
+//             }
+
+//             // swap only once per pass
+//             int temp = nums[i];
+//             nums[i] = nums[minIndex];
+//             nums[minIndex] = temp;
+//         }
+
+//         return nums;
+//     }
+// };
