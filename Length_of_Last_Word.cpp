cf3d9c5d36250f46b4b7ef5644f7d219d5d1912a
Vaibhav262610 <branda56235@gmail.com>
2026-01-29 02:32:48 +0530

    H

---
+++ Length_of_Last_Word.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    string s = "Hello World";
+    cout << s.size();
+    for (char x : s)
+    {
+        cout << s;
+    }
+
+    return 0;
+}
